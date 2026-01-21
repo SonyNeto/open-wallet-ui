8f1ed05c0a51780c4c9b706e3b8a3089c372cebf
felipe1496 <felipe.oliveira1496@gmail.com>
2026-01-21 16:27:30 -0300

    chore: add usePostTransaction endpoint

---
+++ src/hooks/mutations/usePostTransaction.ts
@@ -0,0 +1,17 @@
+import { useMutation } from '@tanstack/react-query';
+import type { MutationOpts } from '../../utils/types';
+import { TransactionsService } from '../../services/TransactionsService';
+
+export function usePostTransaction({
+  mutationKey = [],
+  ...props
+}: MutationOpts<
+  Awaited<ReturnType<typeof TransactionsService.postTransaction>>,
+  Parameters<typeof TransactionsService.postTransaction>[0]
+> = {}) {
+  return useMutation({
+    ...props,
+    mutationKey: ['POST_TRANSACTION_MUTATION', ...mutationKey],
+    mutationFn: TransactionsService.postTransaction,
+  });
+}
